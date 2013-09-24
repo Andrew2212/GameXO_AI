0473b74427a5f2a610339336d677dc5aaa6a96dc
Олег <kolega.ua@gmail.com>
2013-09-24 14:32:17 +0300

    подправил хранение координат победной клетки

--- src/org/hexlet/gamexo/ai/gardnerway/Gardner.java
+++ src/org/hexlet/gamexo/ai/gardnerway/Gardner.java
@@ -73,131 +73,130 @@ public class Gardner implements IBrainAI {
         return gameBoard[column][row] == '\u0000';
     }
 
-    // Проверка на победу
-    public boolean isWin(int x, int y, char chip) {
-        HashSet<String> win = new HashSet<String>();
-        for (int i = 0; i < 4; i++) {
-            win.addAll(checkRow(x, y, chip, i));
+	// Проверка на победу
+	public boolean isWin(int x, int y, char chip) {
+		HashSet<Integer[]> win = new HashSet<>();
+		/*
+		Проход победных рядов по четырем направлениям
+		 */
+		for (int i = 0; i < 4; i++) {
+			win.addAll(checkRow(x, y, chip, i));
 
 			/*
-			Если в куче элементов координат больше, чем один,
+			Если в Set элементов координат больше, чем один,
 			то партия считается выигранной/проигранной, т.к.
-			есть миним одна из двух клеток, которую не успевает
+			есть минимум одна из двух клеток, которую не успевает
 			обезвредить противник.
 			 */
-            if ((win.size()) > 1) {
-                return true;
-            }
-        }
-
-
+			if ((win.size()) > 1) {
+				return true;
+			}
+		}
 		/*
-		Это мне не нравится, надо переделывать.
-		Преобразует координаты в удобоваримый вид из стринги,
-		которая пришла из кучи.
+		Получаем координату потенциалной победной клетки
 		 */
-        String[] coordinate = win.toArray(new String[0]);
-        String[] xy = coordinate[0].split(" ");
-        int[] xAndY = new int[2];
-        for (int i = 0; i < xAndY.length; i++){
-            xAndY[i] = Integer.parseInt(xy[i]);
-            System.out.println(xAndY[i]);
-        }
-        return false;
-    }
+		for (Integer[] integers : win){
+			coordX = integers[0];
+			coordY = integers[1];
+		}
+		return false;
+	}
 
-    /**
-     * В данном методе выполняется проверка победы при заполненнии указанной
-     * фишкой победного ряда состоящего из numInTheRow количества фишек подряд.
-     * При проверке осуществляется перебор полей на длину победного ряда
-     * слева-направо, сверху-вниз, по диагонали слева-вниз и
-     * по диагонали слева-вверх. При этом начальная клетка для проверки
-     * определяется максимальным выносом на длину победного ряда влево или вверх.
-     * После проверки граничной клетки, выполняется проверка на длину ряда для
-     * сосденей с ней клетки в направлении проверки.
-     *
-     * @param xx координата клетки по Х
-     * @param yy координата клетки по Y
-     * @param chip фишка (камень) для которого проверяется состояние выигрыша
-     * @param direction напраление проверки ряда
-     * @return HeshSet координат пустых потенциально победных клеток
-     * @exception IndexOutOfBoundsException выкидывается при попадании
-     * проверочных координат за границы массива.
-     */
-    public HashSet<String> checkRow(int xx, int yy, char chip, int direction) {
-        int x = 0, y = 0;
-        HashSet<String> emptySell = new HashSet<String>();   //сборщик пустых полей от прохода ряда
-        HashSet<String> emptySum = new HashSet<String>();;    //сборщик пустых полей от всех проходов
+	/**
+	 * Выполняется проверка победы при заполненнии указанной фишкой
+	 * победного ряда, состоящего из numInTheRow количества фишек подряд.
+	 * При проверке осуществляется перебор полей на длину победного ряда
+	 * слева-направо, сверху-вниз, по диагонали слева-вниз и
+	 * по диагонали слева-вверх. При этом начальная клетка для проверки
+	 * определяется максимальным выносом на длину победного ряда влево или вверх.
+	 * После проверки граничной клетки, выполняется проверка на длину ряда для
+	 * сосденей с ней клетки в направлении проверки.
+	 *
+	 * @param xx координата клетки по Х
+	 * @param yy координата клетки по Y
+	 * @param chip фишка (камень) для которого проверяется состояние выигрыша
+	 * @param direction напраление проверки ряда
+	 * @return HeshSet координат пустых потенциально победных клеток
+	 * @exception IndexOutOfBoundsException выкидывается при попадании
+	 * проверочных координат за границы массива.
+	 */
+	public HashSet<Integer[]> checkRow(int xx, int yy, char chip, int direction) {
+		int x = 0, y = 0;
+		HashSet<Integer[]> emptySell = new HashSet<>();   //сборщик пустых полей от прохода ряда
+		HashSet<Integer[]> emptySum = new HashSet<>();    //сборщик пустых полей от всех проходов
+		Integer[] xy = new Integer[2];
 
-        start:
-        for (int i = 0; i < numInTheRow; i++) {
-            for (int k = 0; k < numInTheRow; k++) {
-                x = xx - (numInTheRow - 1) + i + k;
-                switch (direction) {      // переключатель направлений
-                    case 0:               // проверка по горизонтали слева
-                        y = yy;
-                        break;
-                    case 1:               // проверка по вертикали сверху
-                        x = xx;
-                        y = yy - (numInTheRow - 1) + i + k;
-                        break;
-                    case 2:               // проверка по диагонали слева-сверху
-                        y = x + (yy - xx);
-                        break;
-                    case 3:               // проверка по диагонали слева-снизу
-                        y = (yy + xx) - x;
-                        break;
-                }
-                try {
+		start:
+		for (int i = 0; i < numInTheRow; i++) {
+			for (int k = 0; k < numInTheRow; k++) {
+				x = xx - (numInTheRow - 1) + i + k;
+				switch (direction) {      // переключатель направлений
+					case 0:               // проверка по горизонтали слева
+						y = yy;
+						break;
+					case 1:               // проверка по вертикали сверху
+						x = xx;
+						y = yy - (numInTheRow - 1) + i + k;
+						break;
+					case 2:               // проверка по диагонали слева-сверху
+						y = x + (yy - xx);
+						break;
+					case 3:               // проверка по диагонали слева-снизу
+						y = (yy + xx) - x;
+						break;
+				}
+				try {
 
 			        /*
 			         Если при проходе попадается знак отличный от
-			        проверяемого, то куча пустых клеток обнуляется и
-			        прекращается дальнейший перебор ряда.
+			         проверяемого, то Set пустых клеток обнуляется и
+			         прекращается дальнейший перебор ряда.
 			         */
-                    if (gameBoard[x][y] != chip & gameBoard[x][y] != '\u0000') {
-                        emptySell.clear();
-                        continue start;
-                    }
+					if (gameBoard[x][y] != chip & gameBoard[x][y] != '\u0000') {
+						emptySell.clear();
+						continue start;
+					}
 			        /*
-			        Здесь мы закидываем пустую клетку в кучу
+			         Здесь мы закидываем пустую клетку в Set
 			         */
-                    if (gameBoard[x][y] == '\u0000') {
-                        emptySell.add(x + " " + y);
-                    }
+					if (gameBoard[x][y] == '\u0000') {
+						xy[0] = x;
+						xy[1] = y;
+						emptySell.add(xy);
+					}
 		        /*
-		        Если проверка попала за границы массива, то обнуляем
-		        кучу пустых клеток.
+		         Если проверка попала за границы массива, то обнуляем
+		         Set пустых клеток.
 		         */
-                } catch (IndexOutOfBoundsException ex) {
-                    emptySell.clear();
-                    continue start;
-                }
-            }
+				} catch (IndexOutOfBoundsException ex) {
+					emptySell.clear();
+					continue start;
+				}
+			}
 	        /*
-	        при отсутствии пустых полей - победа
-	        записываем в кучу больше одного элемента
+	         при отсутствии пустых полей - победа
+	         записываем в кучу больше одного элемента
 	        */
-            if (emptySell.size() == 0) {
-                System.out.println(emptySell.size());
-                for (int g = 0; g < 3; g++) {
-                    emptySum.add("win" + g);
-                }
-                return emptySum;
-            }
+			if (emptySell.size() == 0) {
+				System.out.println(emptySell.size());
+				for (int g = 0; g < 3; g++) {
+					emptySum.add(xy);
+				}
+				return emptySum;
+			}
 	        /*
-	        Если пустых полей больше одного,
-		    то очистить счетчик пустых полей
+	         Если пустых полей больше одного,
+		     то очистить счетчик пустых полей
 		    */
-            if (emptySell.size() > 1) {
-                emptySell.clear();
-            }
-            emptySum.addAll(emptySell);
-            emptySell.clear();
-        }
+			if (emptySell.size() > 1) {
+				emptySell.clear();
+			}
+			emptySum.addAll(emptySell);
+			emptySell.clear();
+		}
 //	    emptySum.addAll(emptySell);
-        return emptySum;
-    }
+		return emptySum;
+	}
 
     public int getCoordC() {
         return coordC;
